5f59330865838b1bda0f2d4be82d7e968a8df0fb
Jinru Wu <woodbleak@gmail.com>
2017-05-08 03:37:07 -0400

    uniqueness validation

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,4 +1,5 @@
 class Measurement < ApplicationRecord
 	belongs_to :unit
 	belongs_to :building
+	validates :unit_id, uniqueness: { scope: [:building_id, :year], message: "should happen once per year" }
 end
